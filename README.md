f681d1af9eb4536730dd333390d42d0217f4354f
wintertwinkle <1070872423@qq.com>
2022-03-21 22:39:21 +0800

    build a little tool

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-Starter-CLI
+
+A little tool helps me to create project in 50-projects-in-react quickly.
---
+++ bin/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk")
+const yargs = require("yargs")
+const path = require("path")
+const fs = require("fs")
+
+const options = yargs.usage("Usage: -n <project-name>").option("n", {
+  alias: "name",
+  describe: "Project name",
+  type: "string",
+  demandOption: true,
+}).argv
+
+const originBasePath = "C:\\Home\\Document\\CS\\前端\\50-projects"
+const targetBasePath =
+  "C:\\Home\\Document\\CS\\前端\\50-projects-in-react\\src\\components"
+
+function generateProject(name) {
+  const originPath = path.join(originBasePath, name)
+  const targetPath = path.join(targetBasePath, name)
+  fs.cp(originPath, targetPath, { recursive: true }, function (err) {
+    if (err) throw err
+    console.log(chalk.green("create project sucess"))
+  })
+}
+
+generateProject(options.name)
+
+// const greeting = chalk.green.bold(`hello, ${options.name}!`)
